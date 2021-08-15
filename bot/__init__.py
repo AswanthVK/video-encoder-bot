d483d4a6c7c57601a7e2018d356acb7a15bc25f8
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-15 14:05:34 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,9 +10,10 @@ api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
-bot_owner = int(os.environ.get("BOT_OWNER"))
+bot_owner = os.environ.get("BOT_OWNER")
 log_channel = int(os.environ.get("LOG_CHANNEL"))
 dump_channel = int(os.environ.get("DUMP_CHANNEL"))
+update_channel = os.environ.get("UPDATE_CHANNEL")
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
